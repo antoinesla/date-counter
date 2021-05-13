76d56d8461ad57fff55fe745d873b2b2ab4e4bed
antoinesla <antoine.agricole@tuta.io>
2021-05-13 21:09:47 +0100

    comments for readability

--- date_calculator.py
+++ date_calculator.py
@@ -53,6 +53,10 @@ def format_timedelta(delta):
 
     return formatted_time_delta
 
+def print_dates(first_date, second_date):
+    '''prints start and end of therapy dates in a nice format'''
+    
+
 def main():
     '''main function that loops and asks for user input'''
     # TODO refactor so that user input is put into a function
@@ -96,19 +100,19 @@ def main():
         if first_date == second_date:
             print('these are the same dates')
         elif first_date > second_date:
+            # write and use print dates method 
             print(f'start of therapy: {first_date}')
             print(f'end of therapy: {second_date}')
             print('first date is further in the futur than second date')
         else:
+            # write and use print dates method 
             delta = second_date - first_date
             print(f'start of therapy: {first_date}')
             print(f'end of therapy: {second_date}')
             print(format_timedelta(delta))
 
 
-        # # resets user_input before restarting loop
-        # user_input = ''
-
+        # wait for user to press enter before starting loop again
         while True:
             input('press enter to restart')
             break
